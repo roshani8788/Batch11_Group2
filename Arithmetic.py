cc22af9bdfe0d93d611160e6adc1531e8cccc5af
roshani8788 <roshanikatekar99@gmail.com>
2023-07-16 19:14:28 +0530

    Added code for multi-Roshani

--- Arithmetic.py
+++ Arithmetic.py
@@ -2,5 +2,11 @@ def add(a, b):
     return a + b
 
 
+def mul(a, b):  # ADDED BY Roshani..
+    return a * b
+
+
 result = add(10, 20)
+result2 = mul(2, 3)
 print(f"Addition of two numbers: {result}")
+print(f"Multiplication of two numbers: {result2}")
