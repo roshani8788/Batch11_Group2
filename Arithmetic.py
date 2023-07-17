29285ffec66e8e5c4c70d1d140574e744280eede
roshani8788 <roshanikatekar99@gmail.com>
2023-07-17 22:07:59 +0530

    CODE REVIEW CHANGES - Neha

--- Arithmetic.py
+++ Arithmetic.py
@@ -2,11 +2,11 @@ def add(a, b):
     return a + b
 
 
-def mul(a, b):  # ADDED BY Roshani..
+def multiplication(a, b):  # ADDED BY Roshani --for code changes..
     return a * b
 
 
 result = add(10, 20)
-result2 = mul(2, 3)
+result2 = multiplication(2, 3)  # ADDED FOR CODE CHANGES
 print(f"Addition of two numbers: {result}")
 print(f"Multiplication of two numbers: {result2}")
